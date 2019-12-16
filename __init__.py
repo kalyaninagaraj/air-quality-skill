40b2129b2fc897cc5a7d4a1ba3997cca6aba036f
kalyaninagaraj <kalyanin@gmail.com>
2019-12-16 14:49:16 +0000

    updated locale

--- __init__.py
+++ __init__.py
@@ -12,11 +12,9 @@ lookup = {
     'pm 10'  : 'pm10',
     'pm2.5'    : 'pm25',
     'pm10'     : 'pm10',
-    'particulate matter'     : 'pm25',
-    'particulate matter 2.5' : 'pm25',
-    'particulate matter 10'  : 'pm10',
-    'fine particulate matter' : 'pm25',
-    'course particulate matter'  : 'pm10',
+    'pm 10'    : 'pm10',
+    'fine particle matter'   : 'pm25',
+    'particle pollution'     : 'pm25',
     'pollution': 'pm25',
     'pollutant': 'pm25',
     'ozone'    : 'o3',
@@ -32,11 +30,8 @@ nice_name = {
     'pm 2.5' : 'pm 2.5',
     'pm10'   : 'pm 10',
     'pm 10'  : 'pm 10',
-    'particulate matter'     : 'pm 2.5',
-    'particulate matter 2.5' : 'pm 25',
-    'particulate matter 10'  : 'pm 10',
-    'fine particulate matter' : 'pm 25',
-    'course particulate matter'  : 'pm 10',
+    'fine particle matter'   : 'pm 2.5',
+    'particle pollution'     : 'pm 2.5',
     'pollution' : 'pm 2.5',
     'pollutant' : 'pm 2.5',
     'ozone'     : 'ozone',
@@ -75,7 +70,7 @@ class AirQuality(MycroftSkill):
                             incity = ''
                         else:
                            incity = 'in ' + city
-                        self.speak_dialog('new.pollutant.level.is', {'pollutant' : nice_name[pollutant],
+                        self.speak_dialog('pollutant.level.is', {'pollutant' : nice_name[pollutant],
                                                                          'incity'    : incity,
                                                                          'value'     : value,
                                                                          'station'   : station})
@@ -89,21 +84,21 @@ class AirQuality(MycroftSkill):
         else:
             self.speak_dialog('key.not.found')
 
-    # Report a US EPA health advisory along with pm 2.5 levels
+    # Report a health advisory along with PM2.5 concentration levels
     # http://aqicn.org/scale/
     def health_advisory(self, value):
         if value <= 50:
             self.speak('The air quality is satisfactory, and air pollution poses little or no risk.')
         elif value <= 100:
-            self.speak('The air quality is acceptable. Some pollutants may pose a moderate health concern for those unusually sensitive to air pollution.')
+            self.speak('The air quality is acceptable. There may be a moderate health concern for individuals who are unusually sensitive to air pollution.')
         elif value <= 150:
-            self.speak('Members of sensitive groups may experience health effects. The general public is not likely to be affected.')
+            self.speak('Members of sensitive groups may experience health effects, but the general public is not likely to be affected.')
         elif value <= 200:
             self.speak('Everyone may begin to experience health effects. Members of sensitive groups may experience more serious health effects.')
         elif value <= 300:
-            self.speak('Everyone may experience more serious health effects.')
+            self.speak('Health alert: everyone may experience more serious health effects.')
         else:
-            self.speak('The entire population is more likely to be affected.')
+            self.speak('Health warning and emergency conditions: the entire population is more likely to be affected by serious health effects.')
 
     # Uses Padatious Intent Handler
     @intent_handler('how.polluted.is.intent')
